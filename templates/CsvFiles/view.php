f9bb038526cf3843fc8a72416c466a79c4ddb678
Angel S. Moreno <angelxmoreno@gmail.com>
2023-04-19 12:24:49 -0400

    rebaked views

--- templates/CsvFiles/view.php
+++ templates/CsvFiles/view.php
@@ -1,48 +1,48 @@
 <?php
 /**
- * @var \App\View\AppView $this
- * @var \App\Model\Entity\CsvFile $csvFile
+* @var AppView $this
+ * @var CsvFile $csvFile
  */
+
+use App\Model\Entity\CsvFile;
+use App\View\AppView;
+use Cake\Collection\CollectionInterface;
+
+$this->extend('/Common/view');
+$this->assign('entity', $csvFile);
+$this->assign('title', 'Csv File');
+$this->assign('subTitle', 'view');
 ?>
-<div class="row">
-    <aside class="column">
-        <div class="side-nav">
-            <h4 class="heading"><?= __('Actions') ?></h4>
-            <?= $this->Html->link(__('Edit Csv File'), ['action' => 'edit', $csvFile->id], ['class' => 'side-nav-item']) ?>
-            <?= $this->Form->postLink(__('Delete Csv File'), ['action' => 'delete', $csvFile->id], ['confirm' => __('Are you sure you want to delete # {0}?', $csvFile->id), 'class' => 'side-nav-item']) ?>
-            <?= $this->Html->link(__('List Csv Files'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
-            <?= $this->Html->link(__('New Csv File'), ['action' => 'add'], ['class' => 'side-nav-item']) ?>
-        </div>
-    </aside>
-    <div class="column-responsive column-80">
-        <div class="csvFiles view content">
-            <h3><?= h($csvFile->name) ?></h3>
-            <table>
-                <tr>
-                    <th><?= __('Name') ?></th>
-                    <td><?= h($csvFile->name) ?></td>
-                </tr>
-                <tr>
-                    <th><?= __('Table Name') ?></th>
-                    <td><?= h($csvFile->table_name) ?></td>
-                </tr>
-                <tr>
-                    <th><?= __('Id') ?></th>
-                    <td><?= $this->Number->format($csvFile->id) ?></td>
-                </tr>
-                <tr>
-                    <th><?= __('Num Rows') ?></th>
-                    <td><?= $this->Number->format($csvFile->num_rows) ?></td>
-                </tr>
-                <tr>
-                    <th><?= __('Created') ?></th>
-                    <td><?= h($csvFile->created) ?></td>
-                </tr>
-                <tr>
-                    <th><?= __('Modified') ?></th>
-                    <td><?= h($csvFile->modified) ?></td>
-                </tr>
-            </table>
-        </div>
+
+
+<div class="csvFiles view large-9 medium-8 columns content">
+    <h4><?= h($csvFile->name) ?></h4>
+    <div class="table-responsive">
+        <table class="table table-striped">
+                                                                        <tr>
+                            <th scope="row"><?= __('Name') ?></th>
+                            <td><?= h($csvFile->name) ?></td>
+                        </tr>
+                                                                                <tr>
+                            <th scope="row"><?= __('Table Name') ?></th>
+                            <td><?= h($csvFile->table_name) ?></td>
+                        </tr>
+                                                                                                            <tr>
+                        <th scope="row"><?= __('Id') ?></th>
+                        <td><?= $this->Number->format($csvFile->id) ?></td>
+                    </tr>
+                                    <tr>
+                        <th scope="row"><?= __('Num Rows') ?></th>
+                        <td><?= $this->Number->format($csvFile->num_rows) ?></td>
+                    </tr>
+                                                                            <tr>
+                        <th scope="row"><?= __('Created') ?></th>
+                        <td><?= h($csvFile->created) ?></td>
+                    </tr>
+                                    <tr>
+                        <th scope="row"><?= __('Modified') ?></th>
+                        <td><?= h($csvFile->modified) ?></td>
+                    </tr>
+                                                </table>
     </div>
-</div>
+            </div>
